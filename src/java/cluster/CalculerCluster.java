1002226299ca063a5066d98d085e5c7f2e6f7c52
GuillaumeLanglois <guillaume.langlois@etu.univ-grenoble-alpes.fr>
2020-05-19 08:11:12 +0200

    setup part 1/2/3/4

--- src/java/cluster/CalculerCluster.java
+++ src/java/cluster/CalculerCluster.java
@@ -3,17 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package cluster;
 
 /**
  *
  * @author guill
  */
-public class Main {
-    
-    public static void main(String[] args){
-        
-        System.out.println("test");
+public class CalculerCluster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
     
 }
